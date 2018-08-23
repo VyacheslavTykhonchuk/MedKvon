7c7bd3f19df0673b13c882ae04449689e19d2695
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2018-08-23 16:41:47 +0300

    updt

--- src/components/switch/Switch.jsx
+++ src/components/switch/Switch.jsx
@@ -1,18 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Switch = ({ isActive, onClick, text }) => {
-  return (
-    <div
-      className={isActive ? "switch-block switch-block_active" : "switch-block"}
-    >
-      {text ? <h5 className="input-block__heading">{text}</h5> : null}
-      <div className="switch-block__switch switch">
-        <div className="switch__circle" />
+class Switch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      isActive: !this.state.isActive
+    });
+  }
+
+  render() {
+    const text = this.props.text;
+    return (
+      <div
+        className={
+          this.state.isActive
+            ? "switch-block switch-block_active"
+            : "switch-block"
+        }
+        onClick={this.handleClick}
+      >
+        {text ? <h5 className="input-block__heading">{text}</h5> : null}
+        <div className="switch-block__switch switch">
+          <div className="switch__circle" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const Switch = ({ isActive, onClick, text }) => {
+//   return (
+//     <div
+//       className={isActive ? "switch-block switch-block_active" : "switch-block"}
+//     >
+//       {text ? <h5 className="input-block__heading">{text}</h5> : null}
+//       <div className="switch-block__switch switch">
+//         <div className="switch__circle" />
+//       </div>
+//     </div>
+//   );
+// };
 
 Switch.propTypes = {
   text: PropTypes.string,
