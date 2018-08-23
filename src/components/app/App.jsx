9e386b3cbeb983dfb5bd06d9c715719558466af9
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2018-08-23 14:33:26 +0300

    icon

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import "./../../assets/styles/style.css";
 
 import Home from "../home/Home";
@@ -10,14 +10,16 @@ import CreateTicket from "../create-ticket/CreateTicket";
 import Wallet from "../wallet/Wallet";
 
 const App = props => (
-  <main>
-    <Route exact path="/" component={Home} />
-    <Route path="/main" component={Main} />
-    <Route path="/account" component={Account} />
-    <Route path="/info-pages" component={InfoPages} />
-    <Route path="/create-ticket" component={CreateTicket} />
-    <Route path="/wallet" component={Wallet} />
-  </main>
+  <HashRouter>
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route path="/main" component={Main} />
+      <Route path="/account" component={Account} />
+      <Route path="/info-pages" component={InfoPages} />
+      <Route path="/create-ticket" component={CreateTicket} />
+      <Route path="/wallet" component={Wallet} />
+    </main>
+  </HashRouter>
 );
 
 export default App;
